4d1b0f9279cc958fe54d937f0c4711da76abed3b
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-21 05:14:49 +0900

    Add implementation
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

---
+++ tests/test_helper.hpp
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: 2026 Kaito Udagawa <umireon@kaito.tokyo>
+//
+// SPDX-License-Identifier: CC0-1.0
+
+#include <cstdint>
+#include <string_view>
+#include <string>
+#include <variant>
+
+#include <KaitoTokyo/SimpleJsonReader/SimpleJsonReader.hpp>
+
+std::string eventTypeToString(KaitoTokyo::SimpleJsonReader::EventType type)
+{
+    using KaitoTokyo::SimpleJsonReader::EventType;
+
+    std::uint8_t bType = static_cast<std::uint8_t>(type);
+
+    if ((bType & 0b01100000) == 0b01100000) {
+        std::uint8_t xIndex = bType & 0b00011111;
+        return "x-" + std::to_string(xIndex);
+    }
+
+    switch (type) {
+        case EventType::Undefined: return "Undefined";
+        case EventType::StartObject: return "StartObject";
+        case EventType::EndObject: return "EndObject";
+        case EventType::StartArray: return "StartArray";
+        case EventType::EndArray: return "EndArray";
+        case EventType::Key: return "Key";
+        case EventType::String: return "String";
+        case EventType::Number: return "Number";
+        case EventType::True: return "True";
+        case EventType::False: return "False";
+        case EventType::Null: return "Null";
+        default: return "(INVALID)";
+    }
+}
+
+std::string jsonPathToString(KaitoTokyo::SimpleJsonReader::JsonPath *jsonPath)
+{
+    std::vector<std::string> pathComponents(jsonPath->depth);
+
+    while (jsonPath != nullptr && jsonPath->parent != nullptr) {
+        if (std::holds_alternative<std::string_view>(jsonPath->component)) {
+            std::string_view fieldName = std::get<std::string_view>(jsonPath->component);
+            pathComponents[jsonPath->depth - 1] = "." + std::string(fieldName);
+        } else {
+            std::size_t index = std::get<std::size_t>(jsonPath->component);
+            pathComponents[jsonPath->depth - 1] = "[" + std::to_string(index) + "]";
+        }
+        jsonPath = jsonPath->parent;
+    }
+
+    if (pathComponents.empty()) {
+        return ".";
+    } else {
+        std::string result;
+        for (const std::string &component : pathComponents) {
+            result += component;
+        }
+        return result;
+    }
+}
